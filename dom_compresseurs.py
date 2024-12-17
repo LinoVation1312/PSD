9e1c3caf86b19d20090f6b9bec84807eb3ba4a80
Lino Conord <158364911+LinoVation1312@users.noreply.github.com>
2024-12-17 17:05:15 +0100

    Update dom_compresseurs.py

--- dom_compresseurs.py
+++ dom_compresseurs.py
@@ -76,10 +76,18 @@ if uploaded_file_1 is not None and uploaded_file_2 is not None:
             st.pyplot(fig)
             
             st.markdown("""
-            “**La PSD (Densité Spectrale de Puissance)** est une représentation du signal dans le **domaine fréquentiel**, contrairement à l'analyse temporelle qui mesure l'amplitude du signal à un instant donné. Elle montre **comment la puissance du signal est répartie sur différentes fréquences**. En d'autres termes, la PSD permet de savoir quelle quantité de puissance est concentrée dans chaque plage de fréquences, donnant ainsi une vue détaillée de l'énergie présente dans le signal à travers le temps et les fréquences.”
 
 
-            """)
+            
+            <p style="text-align: justify;">
+                <strong>La PSD (Densité Spectrale de Puissance)</strong> est une représentation du signal dans le 
+                <strong>domaine fréquentiel</strong>, contrairement à l'analyse temporelle qui mesure l'amplitude du signal 
+                à un instant donné. Elle montre <strong>comment la puissance du signal est répartie sur différentes fréquences</strong>. 
+                En d'autres termes, la PSD permet de savoir quelle quantité de puissance est concentrée dans chaque plage de 
+                fréquences, donnant ainsi une vue détaillée de l'énergie présente dans le signal à travers le temps et les fréquences.
+            </p>
+            """, unsafe_allow_html=True)
+
             # --- 2. Extraction de la fréquence dominante optimisée ---
             def extract_fundamental_frequency(y, sr):
                 # Calcul de la FFT
